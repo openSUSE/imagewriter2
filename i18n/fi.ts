ab679687e326a5ae22340d419f42b6ab5bd44a9d
Tommi Nieminen <software@legisign.org>
2018-02-20 15:29:37 +0000

    Translated using Weblate (Finnish)
    Currently translated at 100.0% (81 of 81 strings)
    
    Translation: imagewriter2/master
    Translate-URL: https://l10n.opensuse.org/projects/imagewriter2/master/fi/

--- i18n/fi.ts
+++ i18n/fi.ts
@@ -6,17 +6,17 @@
     <message>
         <location filename="../AboutDialog.qml" line="18"/>
         <source>About openSUSE Image Writer</source>
-        <translation></translation>
+        <translation>Tietoa openSUSEn levykuvakirjoittimesta</translation>
     </message>
     <message>
         <location filename="../AboutDialog.qml" line="54"/>
         <source>About</source>
-        <translation></translation>
+        <translation>Tietoa</translation>
     </message>
     <message>
         <location filename="../AboutDialog.qml" line="63"/>
         <source>Settings</source>
-        <translation></translation>
+        <translation>Asetukset</translation>
     </message>
 </context>
 <context>
@@ -29,12 +29,12 @@
     <message>
         <location filename="../AboutView.qml" line="34"/>
         <source>Homepage: &lt;a href=&quot;%1&quot;&gt;On GitHub&lt;/a&gt;&lt;br/&gt;An &lt;a href=&quot;%2&quot;&gt;openSUSE&lt;/a&gt; project&lt;br/&gt;Created by Fabian Vogt</source>
-        <translation></translation>
+        <translation>Kotisivu: &lt;a href=&quot;%1&quot;&gt;GitHub&lt;/a&gt;issa&lt;br/&gt;Fabian Vogtin luoma&lt;br/&gt;&lt;a href=&quot;%2&quot;&gt;openSUSE&lt;/a&gt;-projekti</translation>
     </message>
     <message>
         <location filename="../AboutView.qml" line="58"/>
         <source>Close</source>
-        <translation></translation>
+        <translation>Sulje</translation>
     </message>
 </context>
 <context>
@@ -42,32 +42,32 @@
     <message>
         <location filename="../cdrecordburntask.cpp" line="7"/>
         <source>Writing to %1</source>
-        <translation></translation>
+        <translation>Kirjoitetaan kohteeseen %1</translation>
     </message>
     <message>
         <location filename="../cdrecordburntask.cpp" line="36"/>
         <source>Unable to find the SCSI device</source>
-        <translation></translation>
+        <translation>SCSI-laitetta ei löydy</translation>
     </message>
     <message>
         <location filename="../cdrecordburntask.cpp" line="61"/>
         <source>Aborted</source>
-        <translation></translation>
+        <translation>Keskeytetty</translation>
     </message>
     <message>
         <location filename="../cdrecordburntask.cpp" line="69"/>
         <source>Writing successful</source>
-        <translation></translation>
+        <translation>Kirjoittaminen onnistui</translation>
     </message>
     <message>
         <location filename="../cdrecordburntask.cpp" line="76"/>
         <source>Writing failed</source>
-        <translation></translation>
+        <translation>Kirjoittaminen epäonnistui</translation>
     </message>
     <message>
         <location filename="../cdrecordburntask.cpp" line="78"/>
         <source>Writing failed: %1</source>
-        <translation></translation>
+        <translation>Kirjoittaminen epäonnistui: %1</translation>
     </message>
 </context>
 <context>
@@ -75,57 +75,57 @@
     <message>
         <location filename="../gpgchecksumtask.cpp" line="12"/>
         <source>Downloading checksum</source>
-        <translation></translation>
+        <translation>Ladataan tarkistussummaa</translation>
     </message>
     <message>
         <location filename="../gpgchecksumtask.cpp" line="45"/>
         <source>Failed to open GPG keyring</source>
-        <translation></translation>
+        <translation>GPG-avainrenkaan avaaminen epäonnistui</translation>
     </message>
     <message>
         <location filename="../gpgchecksumtask.cpp" line="57"/>
         <source>Download skipped, checksum verified</source>
-        <translation></translation>
+        <translation>Ohitettiin lataus, tarkistussumma tarkistettu</translation>
     </message>
     <message>
         <location filename="../gpgchecksumtask.cpp" line="71"/>
         <source>No network connectivity</source>
-        <translation></translation>
+        <translation>Ei verkkoyhteyttä</translation>
     </message>
     <message>
         <location filename="../gpgchecksumtask.cpp" line="80"/>
         <source>Could not create target file</source>
-        <translation></translation>
+        <translation>Kohdetiedostoa ei voida luoda</translation>
     </message>
     <message>
         <location filename="../gpgchecksumtask.cpp" line="90"/>
         <source>Starting checksum download</source>
-        <translation></translation>
+        <translation>Aloitetaan tarkistussumman lataus</translation>
     </message>
     <message>
         <location filename="../gpgchecksumtask.cpp" line="108"/>
         <source>Aborted</source>
-        <translation></translation>
+        <translation>Keskeytetty</translation>
     </message>
     <message>
         <location filename="../gpgchecksumtask.cpp" line="126"/>
         <source>Download failed: %1</source>
-        <translation></translation>
+        <translation>Lataus epäonnistui: %1</translation>
     </message>
     <message>
         <location filename="../gpgchecksumtask.cpp" line="147"/>
         <source>Failed to verify signature of checksum</source>
-        <translation></translation>
+        <translation>Tarkistussumman allekirjoituksen todennus epäonnistui</translation>
     </message>
     <message>
         <location filename="../gpgchecksumtask.cpp" line="152"/>
         <source>Download finished, could not rename</source>
-        <translation></translation>
+        <translation>Lataus valmis, nimeä ei voitu muuttaa</translation>
     </message>
     <message>
         <location filename="../gpgchecksumtask.cpp" line="158"/>
         <source>Checksum downloaded and verified</source>
-        <translation></translation>
+        <translation>Tarkistussumma ladattu ja tarkistettu</translation>
     </message>
 </context>
 <context>
@@ -133,7 +133,7 @@
     <message>
         <location filename="../imagedownloadtask.cpp" line="14"/>
         <source>Downloading %1</source>
-        <translation></translation>
+        <translation>Ladataan: %1</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="58"/>
@@ -143,52 +143,52 @@
     <message>
         <location filename="../imagedownloadtask.cpp" line="88"/>
         <source>Aborted</source>
-        <translation></translation>
+        <translation>Keskeytetty</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="114"/>
         <source>Download failed: %1</source>
-        <translation></translation>
+        <translation>Lataus epäonnistui: %1</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="134"/>
         <source>Download finished, but checksum failed</source>
-        <translation></translation>
+        <translation>Lataus valmis mutta tarkistussumma epäonnistui</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="139"/>
         <source>Download finished, could not rename</source>
-        <translation></translation>
+        <translation>Lataus valmis, nimeä ei voi muuttaa</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="145"/>
         <source>Download done</source>
-        <translation></translation>
+        <translation>Lataus valmis</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="162"/>
         <source>No checksum available for this image</source>
-        <translation></translation>
+        <translation>Levykuvalle ei ole saatavilla tarkistussummaa</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="170"/>
         <source>Download skipped, found in cache</source>
-        <translation></translation>
+        <translation>Ohitettiin lataus: löytyi välimuistista</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="178"/>
         <source>No network connectivity</source>
-        <translation></translation>
+        <translation>Ei verkkoyhteyttä</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="187"/>
         <source>Could not create target file</source>
-        <translation></translation>
+        <translation>Kohdetiedostoa ei voitu luoda</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="197"/>
         <source>Starting download</source>
-        <translation></translation>
+        <translation>Käynnistetään latausta</translation>
     </message>
 </context>
 <context>
@@ -196,27 +196,27 @@
     <message>
         <location filename="../imagedownloaderwritertask.cpp" line="39"/>
         <source>Downloading image</source>
-        <translation></translation>
+        <translation>Ladataan levykuvaa</translation>
     </message>
     <message>
         <location filename="../imagedownloaderwritertask.cpp" line="60"/>
         <source>Writing image</source>
-        <translation></translation>
+        <translation>Kirjoitetaan levykuvaa</translation>
     </message>
     <message>
         <location filename="../imagedownloaderwritertask.cpp" line="72"/>
         <source>Downloading failed</source>
-        <translation></translation>
+        <translation>Lataus epäonnistui</translation>
     </message>
     <message>
         <location filename="../imagedownloaderwritertask.cpp" line="99"/>
         <source>Done</source>
-        <translation></translation>
+        <translation>Valmis</translation>
     </message>
     <message>
         <location filename="../imagedownloaderwritertask.cpp" line="104"/>
         <source>Writing failed</source>
-        <translation></translation>
+        <translation>Kirjoittaminen epäonnistui</translation>
     </message>
     <message>
         <location filename="../imagedownloaderwritertask.cpp" line="109"/>
@@ -229,37 +229,37 @@
     <message>
         <location filename="../metadatadownloadtask.cpp" line="9"/>
         <source>Getting information about %1 images</source>
-        <translation></translation>
+        <translation>Noudetaan tietoja %1 levykuvasta</translation>
     </message>
     <message>
         <location filename="../metadatadownloadtask.cpp" line="27"/>
         <source>Downloading metadata</source>
-        <translation></translation>
+        <translation>Ladataan metatietoja</translation>
     </message>
     <message>
         <location filename="../metadatadownloadtask.cpp" line="45"/>
         <source>Aborted</source>
-        <translation></translation>
+        <translation>Keskeytetty</translation>
     </message>
     <message>
         <location filename="../metadatadownloadtask.cpp" line="69"/>
         <source>Metadata download failed, using cache</source>
-        <translation></translation>
+        <translation>Metatietojen lataus epäonnistui: käytetään välimuistia</translation>
     </message>
     <message>
         <location filename="../metadatadownloadtask.cpp" line="75"/>
         <source>Metadata download failed, cache not available</source>
-        <translation></translation>
+        <translation>Metatietojen lataus epäonnistui eikä välimuistia ole käytettävissä</translation>
     </message>
     <message>
         <location filename="../metadatadownloadtask.cpp" line="87"/>
         <source>Metadata downloaded</source>
-        <translation></translation>
+        <translation>Metatiedot ladattu</translation>
     </message>
     <message>
         <location filename="../metadatadownloadtask.cpp" line="94"/>
         <source>Failed to save metadata</source>
-        <translation></translation>
+        <translation>Metatietojen tallennus epäonnistui</translation>
     </message>
 </context>
 <context>
@@ -290,22 +290,22 @@
     <message>
         <location filename="../removabledevicesmodeludisks2.cpp" line="248"/>
         <source>%1 DVD at %2</source>
-        <translation></translation>
+        <translation>%1-DVD kohteessa %2</translation>
     </message>
     <message>
         <location filename="../removabledevicesmodeludisks2.cpp" line="251"/>
         <source>%1 Harddisk at %2</source>
-        <translation></translation>
+        <translation>%1-kiintolevy kohteessa %2</translation>
     </message>
     <message>
         <location filename="../removabledevicesmodeludisks2.cpp" line="254"/>
         <source>%1 USB Drive at %2</source>
-        <translation></translation>
+        <translation>%1-USB-asema kohteessa %2</translation>
     </message>
     <message>
         <location filename="../removabledevicesmodeludisks2.cpp" line="257"/>
         <source>Unknown %1 device at %2</source>
-        <translation></translation>
+        <translation>Tuntematon %1-laite kohteessa %2</translation>
     </message>
 </context>
 <context>
@@ -313,17 +313,17 @@
     <message>
         <location filename="../SettingsView.qml" line="32"/>
         <source>Only download images (this session only)</source>
-        <translation></translation>
+        <translation>Lataa vain levykuvat (vain tämä istunto)</translation>
     </message>
     <message>
         <location filename="../SettingsView.qml" line="44"/>
         <source>Cache:</source>
-        <translation></translation>
+        <translation>Välimuisti:</translation>
     </message>
     <message>
         <location filename="../SettingsView.qml" line="61"/>
         <source>Clear cache</source>
-        <translation></translation>
+        <translation>Tyhjennä välimuisti</translation>
     </message>
 </context>
 <context>
@@ -331,7 +331,7 @@
     <message>
         <location filename="../usbimagewritertask.cpp" line="73"/>
         <source>Writing %1 to %2</source>
-        <translation></translation>
+        <translation>Kirjoitetaan %1 kohteeseen %2</translation>
     </message>
     <message>
         <location filename="../usbimagewritertask.cpp" line="102"/>
@@ -341,22 +341,22 @@
     <message>
         <location filename="../usbimagewritertask.cpp" line="108"/>
         <source>Starting write</source>
-        <translation></translation>
+        <translation>Aletaan kirjoittaa</translation>
     </message>
     <message>
         <location filename="../usbimagewritertask.cpp" line="127"/>
         <source>Aborted</source>
-        <translation></translation>
+        <translation>Keskeytetty</translation>
     </message>
     <message>
         <location filename="../usbimagewritertask.cpp" line="150"/>
         <source>Writing successful</source>
-        <translation></translation>
+        <translation>Kirjoittaminen onnistui</translation>
     </message>
     <message>
         <location filename="../usbimagewritertask.cpp" line="155"/>
         <source>Writing failed: %1</source>
-        <translation></translation>
+        <translation>Kirjoittaminen epäonnistui: %1</translation>
     </message>
 </context>
 <context>
@@ -369,80 +369,83 @@
     <message>
         <location filename="../main.qml" line="49"/>
         <source>Could not load metadata</source>
-        <translation></translation>
+        <translation>Metatietoja ei voitu ladata</translation>
     </message>
     <message>
         <location filename="../main.qml" line="50"/>
         <source>The downloaded Metadata was invalid.</source>
-        <translation></translation>
+        <translation>Ladatut metatiedot olivat virheellisiä.</translation>
     </message>
     <message>
         <location filename="../main.qml" line="128"/>
         <source>Source Image</source>
-        <translation></translation>
+        <translation>Lähdelevykuva</translation>
     </message>
     <message>
         <location filename="../main.qml" line="232"/>
         <source>No valid image selected</source>
-        <translation></translation>
+        <translation>Kelvollista levykuvaa ei ole valittu</translation>
     </message>
     <message>
         <location filename="../main.qml" line="242"/>
         <source>No valid drive selected</source>
-        <translation></translation>
+        <translation>Kelvollista asemaa ei ole valittu</translation>
     </message>
     <message>
         <location filename="../main.qml" line="249"/>
         <source>The selected drive is too small for the image</source>
-        <translation></translation>
+        <translation>Valittu asema on levykuvalle liian pieni</translation>
     </message>
     <message>
         <location filename="../main.qml" line="255"/>
         <source>Ready to write!</source>
-        <translation></translation>
+        <translation>Valmiina kirjoittamaan!</translation>
     </message>
     <message>
         <location filename="../main.qml" line="308"/>
         <source>Overwrite data?</source>
-        <translation></translation>
+        <translation>Korvataanko tiedot?</translation>
     </message>
     <message>
         <location filename="../main.qml" line="309"/>
         <source>By continuing here, you will LOSE ALL DATA on the Device
 %1!
 Are you sure?</source>
-        <translation></translation>
+        <translation>Jos jatkat, MENETÄT KAIKKI TIEDOT laitteelta
+%1!
+Oletko varma?</translation>
     </message>
     <message>
         <location filename="../main.qml" line="326"/>
         <source>Could not start task</source>
-        <translation></translation>
+        <translation>Tehtävää ei voitu käynnistää</translation>
     </message>
     <message>
         <location filename="../main.qml" line="327"/>
         <source>Opening of %1 for writing failed:
 %2</source>
-        <translation></translation>
+        <translation>Laitteen %1 avaaminen kirjoitettavaksi epäonnistui:
+%2</translation>
     </message>
     <message>
         <location filename="../main.qml" line="349"/>
         <source>Start!</source>
-        <translation></translation>
+        <translation>Aloita!</translation>
     </message>
     <message>
         <location filename="../main.qml" line="394"/>
         <source>Target Disk</source>
-        <translation></translation>
+        <translation>Kohdelevy</translation>
     </message>
     <message>
         <location filename="../main.qml" line="462"/>
         <source>Task List</source>
-        <translation></translation>
+        <translation>Tehtäväluettelo</translation>
     </message>
     <message>
         <location filename="../main.qml" line="472"/>
         <source>About openSUSE ImageWriter</source>
-        <translation></translation>
+        <translation>Tietoa openSUSE ImageWriterista</translation>
     </message>
 </context>
 </TS>
