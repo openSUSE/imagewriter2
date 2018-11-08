5ecfccb91697ef47505c66baee8a0895b12d0338
Alex Minton <alex239@gmail.com>
2018-11-08 20:14:08 +0000

    Translated using Weblate (Russian)
    Currently translated at 100.0% (81 of 81 strings)
    
    Translation: imagewriter2/master
    Translate-URL: https://l10n.opensuse.org/projects/imagewriter2/master/ru/

--- i18n/ru.ts
+++ i18n/ru.ts
@@ -6,17 +6,17 @@
     <message>
         <location filename="../AboutDialog.qml" line="18"/>
         <source>About openSUSE Image Writer</source>
-        <translation></translation>
+        <translation>О программе openSUSE Image Writer</translation>
     </message>
     <message>
         <location filename="../AboutDialog.qml" line="54"/>
         <source>About</source>
-        <translation></translation>
+        <translation>О программе</translation>
     </message>
     <message>
         <location filename="../AboutDialog.qml" line="63"/>
         <source>Settings</source>
-        <translation></translation>
+        <translation>Настройки</translation>
     </message>
 </context>
 <context>
@@ -29,12 +29,12 @@
     <message>
         <location filename="../AboutView.qml" line="34"/>
         <source>Homepage: &lt;a href=&quot;%1&quot;&gt;On GitHub&lt;/a&gt;&lt;br/&gt;An &lt;a href=&quot;%2&quot;&gt;openSUSE&lt;/a&gt; project&lt;br/&gt;Created by Fabian Vogt</source>
-        <translation></translation>
+        <translation>Домашняя страница: &lt;a href=&quot;%1&quot;&gt;на GitHub&lt;/a&gt;&lt;br/&gt;Проект &lt;a href=&quot;%2&quot;&gt;openSUSE&lt;/a&gt;&lt;br/&gt;Автор Fabian Vogt</translation>
     </message>
     <message>
         <location filename="../AboutView.qml" line="58"/>
         <source>Close</source>
-        <translation></translation>
+        <translation>Закрыть</translation>
     </message>
 </context>
 <context>
@@ -42,32 +42,32 @@
     <message>
         <location filename="../cdrecordburntask.cpp" line="7"/>
         <source>Writing to %1</source>
-        <translation></translation>
+        <translation>Запись на %1</translation>
     </message>
     <message>
         <location filename="../cdrecordburntask.cpp" line="36"/>
         <source>Unable to find the SCSI device</source>
-        <translation></translation>
+        <translation>Не могу найти SCSI-устройство</translation>
     </message>
     <message>
         <location filename="../cdrecordburntask.cpp" line="61"/>
         <source>Aborted</source>
-        <translation></translation>
+        <translation>Прервано</translation>
     </message>
     <message>
         <location filename="../cdrecordburntask.cpp" line="69"/>
         <source>Writing successful</source>
-        <translation></translation>
+        <translation>Записано успешно</translation>
     </message>
     <message>
         <location filename="../cdrecordburntask.cpp" line="76"/>
         <source>Writing failed</source>
-        <translation></translation>
+        <translation>Сбой при записи</translation>
     </message>
     <message>
         <location filename="../cdrecordburntask.cpp" line="78"/>
         <source>Writing failed: %1</source>
-        <translation></translation>
+        <translation>Сбой при записи: %1</translation>
     </message>
 </context>
 <context>
@@ -75,57 +75,57 @@
     <message>
         <location filename="../gpgchecksumtask.cpp" line="12"/>
         <source>Downloading checksum</source>
-        <translation></translation>
+        <translation>Скачивание контрольной суммы</translation>
     </message>
     <message>
         <location filename="../gpgchecksumtask.cpp" line="45"/>
         <source>Failed to open GPG keyring</source>
-        <translation></translation>
+        <translation>Сбой при открытии связки ключей GPG</translation>
     </message>
     <message>
         <location filename="../gpgchecksumtask.cpp" line="57"/>
         <source>Download skipped, checksum verified</source>
-        <translation></translation>
+        <translation>Скачивание пропущено, контрольная сумма проверена</translation>
     </message>
     <message>
         <location filename="../gpgchecksumtask.cpp" line="71"/>
         <source>No network connectivity</source>
-        <translation></translation>
+        <translation>Нет подключения к сети</translation>
     </message>
     <message>
         <location filename="../gpgchecksumtask.cpp" line="80"/>
         <source>Could not create target file</source>
-        <translation></translation>
+        <translation>Не могу создать целевой файл</translation>
     </message>
     <message>
         <location filename="../gpgchecksumtask.cpp" line="90"/>
         <source>Starting checksum download</source>
-        <translation></translation>
+        <translation>Начинаю скачивание контрольной суммы</translation>
     </message>
     <message>
         <location filename="../gpgchecksumtask.cpp" line="108"/>
         <source>Aborted</source>
-        <translation></translation>
+        <translation>Прервано</translation>
     </message>
     <message>
         <location filename="../gpgchecksumtask.cpp" line="126"/>
         <source>Download failed: %1</source>
-        <translation></translation>
+        <translation>Сбой при скачивании: %1</translation>
     </message>
     <message>
         <location filename="../gpgchecksumtask.cpp" line="147"/>
         <source>Failed to verify signature of checksum</source>
-        <translation></translation>
+        <translation>Сбой при проверке подписи контрольной суммы</translation>
     </message>
     <message>
         <location filename="../gpgchecksumtask.cpp" line="152"/>
         <source>Download finished, could not rename</source>
-        <translation></translation>
+        <translation>Скачивание завершено, не могу переименовать</translation>
     </message>
     <message>
         <location filename="../gpgchecksumtask.cpp" line="158"/>
         <source>Checksum downloaded and verified</source>
-        <translation></translation>
+        <translation>Контрольная сумма скачана и проверена</translation>
     </message>
 </context>
 <context>
@@ -133,62 +133,62 @@
     <message>
         <location filename="../imagedownloadtask.cpp" line="14"/>
         <source>Downloading %1</source>
-        <translation></translation>
+        <translation>Скачивание %1</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="58"/>
         <source>%1 / %2 (%3/s)</source>
-        <translation></translation>
+        <translation>%1 / %2 (%3/с)</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="88"/>
         <source>Aborted</source>
-        <translation></translation>
+        <translation>Прервано</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="114"/>
         <source>Download failed: %1</source>
-        <translation></translation>
+        <translation>Сбой при загрузке: %1</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="134"/>
         <source>Download finished, but checksum failed</source>
-        <translation></translation>
+        <translation>Скачивание завершено, сбой проверки контрольной суммы</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="139"/>
         <source>Download finished, could not rename</source>
-        <translation></translation>
+        <translation>Скачивание завершено, не могу переименовать</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="145"/>
         <source>Download done</source>
-        <translation></translation>
+        <translation>Скачивание выполнено</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="162"/>
         <source>No checksum available for this image</source>
-        <translation></translation>
+        <translation>Для этого образа отсутствует контрольная сумма</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="170"/>
         <source>Download skipped, found in cache</source>
-        <translation></translation>
+        <translation>Скачивание пропущено, найдено в кэше</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="178"/>
         <source>No network connectivity</source>
-        <translation></translation>
+        <translation>Нет подключения к сети</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="187"/>
         <source>Could not create target file</source>
-        <translation></translation>
+        <translation>Не могу создать целевой файл</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="197"/>
         <source>Starting download</source>
-        <translation></translation>
+        <translation>Начинаю скачивание</translation>
     </message>
 </context>
 <context>
@@ -196,32 +196,32 @@
     <message>
         <location filename="../imagedownloaderwritertask.cpp" line="39"/>
         <source>Downloading image</source>
-        <translation></translation>
+        <translation>Скачивание образа</translation>
     </message>
     <message>
         <location filename="../imagedownloaderwritertask.cpp" line="60"/>
         <source>Writing image</source>
-        <translation></translation>
+        <translation>Запись образа</translation>
     </message>
     <message>
         <location filename="../imagedownloaderwritertask.cpp" line="72"/>
         <source>Downloading failed</source>
-        <translation></translation>
+        <translation>Сбой при скачивании</translation>
     </message>
     <message>
         <location filename="../imagedownloaderwritertask.cpp" line="99"/>
         <source>Done</source>
-        <translation></translation>
+        <translation>Выполнено</translation>
     </message>
     <message>
         <location filename="../imagedownloaderwritertask.cpp" line="104"/>
         <source>Writing failed</source>
-        <translation></translation>
+        <translation>Сбой при записи</translation>
     </message>
     <message>
         <location filename="../imagedownloaderwritertask.cpp" line="109"/>
         <source>Deploying %1 to %2</source>
-        <translation></translation>
+        <translation>Развёртывание %1 на %2</translation>
     </message>
 </context>
 <context>
@@ -229,37 +229,37 @@
     <message>
         <location filename="../metadatadownloadtask.cpp" line="9"/>
         <source>Getting information about %1 images</source>
-        <translation></translation>
+        <translation>Получение информации об образах: %1</translation>
     </message>
     <message>
         <location filename="../metadatadownloadtask.cpp" line="27"/>
         <source>Downloading metadata</source>
-        <translation></translation>
+        <translation>Скачивание метаданных</translation>
     </message>
     <message>
         <location filename="../metadatadownloadtask.cpp" line="45"/>
         <source>Aborted</source>
-        <translation></translation>
+        <translation>Прервано</translation>
     </message>
     <message>
         <location filename="../metadatadownloadtask.cpp" line="69"/>
         <source>Metadata download failed, using cache</source>
-        <translation></translation>
+        <translation>Сбой скачивания метаданных, использую кэш</translation>
     </message>
     <message>
         <location filename="../metadatadownloadtask.cpp" line="75"/>
         <source>Metadata download failed, cache not available</source>
-        <translation></translation>
+        <translation>Сбой скачивания метаданных, кэш недоступен</translation>
     </message>
     <message>
         <location filename="../metadatadownloadtask.cpp" line="87"/>
         <source>Metadata downloaded</source>
-        <translation></translation>
+        <translation>Метаданные загружены</translation>
     </message>
     <message>
         <location filename="../metadatadownloadtask.cpp" line="94"/>
         <source>Failed to save metadata</source>
-        <translation></translation>
+        <translation>Сбой при сохранении метаданных</translation>
     </message>
 </context>
 <context>
@@ -267,22 +267,22 @@
     <message>
         <location filename="../qml64sizetype.h" line="42"/>
         <source>%L1 GiB</source>
-        <translation></translation>
+        <translation>%L1 ГиБ</translation>
     </message>
     <message>
         <location filename="../qml64sizetype.h" line="44"/>
         <source>%L1 MiB</source>
-        <translation></translation>
+        <translation>%L1 МиБ</translation>
     </message>
     <message>
         <location filename="../qml64sizetype.h" line="46"/>
         <source>%L1 KiB</source>
-        <translation></translation>
+        <translation>%L1 КиБ</translation>
     </message>
     <message>
         <location filename="../qml64sizetype.h" line="48"/>
         <source>%L1 B</source>
-        <translation></translation>
+        <translation>%L1 Б</translation>
     </message>
 </context>
 <context>
@@ -290,22 +290,22 @@
     <message>
         <location filename="../removabledevicesmodeludisks2.cpp" line="248"/>
         <source>%1 DVD at %2</source>
-        <translation></translation>
+        <translation>%1 DVD на %2</translation>
     </message>
     <message>
         <location filename="../removabledevicesmodeludisks2.cpp" line="251"/>
         <source>%1 Harddisk at %2</source>
-        <translation></translation>
+        <translation>%1 жёсткий диск на %2</translation>
     </message>
     <message>
         <location filename="../removabledevicesmodeludisks2.cpp" line="254"/>
         <source>%1 USB Drive at %2</source>
-        <translation></translation>
+        <translation>%1 USB-накопитель на %2</translation>
     </message>
     <message>
         <location filename="../removabledevicesmodeludisks2.cpp" line="257"/>
         <source>Unknown %1 device at %2</source>
-        <translation></translation>
+        <translation>Неизвестное устройство %1 на %2</translation>
     </message>
 </context>
 <context>
@@ -313,17 +313,17 @@
     <message>
         <location filename="../SettingsView.qml" line="32"/>
         <source>Only download images (this session only)</source>
-        <translation></translation>
+        <translation>Только скачать образы (только этот сеанс)</translation>
     </message>
     <message>
         <location filename="../SettingsView.qml" line="44"/>
         <source>Cache:</source>
-        <translation></translation>
+        <translation>Кэш:</translation>
     </message>
     <message>
         <location filename="../SettingsView.qml" line="61"/>
         <source>Clear cache</source>
-        <translation></translation>
+        <translation>Очистить кэш</translation>
     </message>
 </context>
 <context>
@@ -331,32 +331,32 @@
     <message>
         <location filename="../usbimagewritertask.cpp" line="73"/>
         <source>Writing %1 to %2</source>
-        <translation></translation>
+        <translation>Запись %1 на %2</translation>
     </message>
     <message>
         <location filename="../usbimagewritertask.cpp" line="102"/>
         <source>%1 / %2 (%3/s)</source>
-        <translation></translation>
+        <translation>%1 / %2 (%3/с)</translation>
     </message>
     <message>
         <location filename="../usbimagewritertask.cpp" line="108"/>
         <source>Starting write</source>
-        <translation></translation>
+        <translation>Начинаю запись</translation>
     </message>
     <message>
         <location filename="../usbimagewritertask.cpp" line="127"/>
         <source>Aborted</source>
-        <translation></translation>
+        <translation>Прервано</translation>
     </message>
     <message>
         <location filename="../usbimagewritertask.cpp" line="150"/>
         <source>Writing successful</source>
-        <translation></translation>
+        <translation>Записано успешно</translation>
     </message>
     <message>
         <location filename="../usbimagewritertask.cpp" line="155"/>
         <source>Writing failed: %1</source>
-        <translation></translation>
+        <translation>Сбой при записи: %1</translation>
     </message>
 </context>
 <context>
@@ -369,80 +369,83 @@
     <message>
         <location filename="../main.qml" line="49"/>
         <source>Could not load metadata</source>
-        <translation></translation>
+        <translation>Не могу загрузить метаданные</translation>
     </message>
     <message>
         <location filename="../main.qml" line="50"/>
         <source>The downloaded Metadata was invalid.</source>
-        <translation></translation>
+        <translation>Скачанные метаданные некорректны.</translation>
     </message>
     <message>
         <location filename="../main.qml" line="128"/>
         <source>Source Image</source>
-        <translation></translation>
+        <translation>Образ-источник</translation>
     </message>
     <message>
         <location filename="../main.qml" line="232"/>
         <source>No valid image selected</source>
-        <translation></translation>
+        <translation>Не выбрано корректного образа</translation>
     </message>
     <message>
         <location filename="../main.qml" line="242"/>
         <source>No valid drive selected</source>
-        <translation></translation>
+        <translation>Не выбрано корректного устройства</translation>
     </message>
     <message>
         <location filename="../main.qml" line="249"/>
         <source>The selected drive is too small for the image</source>
-        <translation></translation>
+        <translation>Выбранное устройство слишком мало для этого образа</translation>
     </message>
     <message>
         <location filename="../main.qml" line="255"/>
         <source>Ready to write!</source>
-        <translation></translation>
+        <translation>Готов к записи!</translation>
     </message>
     <message>
         <location filename="../main.qml" line="308"/>
         <source>Overwrite data?</source>
-        <translation></translation>
+        <translation>Перезаписать данные?</translation>
     </message>
     <message>
         <location filename="../main.qml" line="309"/>
         <source>By continuing here, you will LOSE ALL DATA on the Device
 %1!
 Are you sure?</source>
-        <translation></translation>
+        <translation>Продолжив, вы потеряете ВСЕ ДАННЫЕ на устройстве
+%1!
+Вы уверены?</translation>
     </message>
     <message>
         <location filename="../main.qml" line="326"/>
         <source>Could not start task</source>
-        <translation></translation>
+        <translation>Не могу запустить задачу</translation>
     </message>
     <message>
         <location filename="../main.qml" line="327"/>
         <source>Opening of %1 for writing failed:
 %2</source>
-        <translation></translation>
+        <translation>Сбой при открытии %1 на запись:
+%2</translation>
     </message>
     <message>
         <location filename="../main.qml" line="349"/>
         <source>Start!</source>
-        <translation></translation>
+        <translation>Начать!</translation>
     </message>
     <message>
         <location filename="../main.qml" line="394"/>
         <source>Target Disk</source>
-        <translation></translation>
+        <translation>Целевой диск</translation>
     </message>
     <message>
         <location filename="../main.qml" line="462"/>
         <source>Task List</source>
-        <translation></translation>
+        <translation>Список задач</translation>
     </message>
     <message>
         <location filename="../main.qml" line="472"/>
         <source>About openSUSE ImageWriter</source>
-        <translation></translation>
+        <translation>О программе openSUSE ImageWriter</translation>
     </message>
 </context>
 </TS>
