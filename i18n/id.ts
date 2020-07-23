80b2c3a3173309c6b93d51a64eb41ccbebb87a8e
Arif Budiman <arifpedia@gmail.com>
2020-07-23 05:45:24 +0000

    Translated using Weblate (Indonesian)
    Currently translated at 79.0% (64 of 81 strings)
    
    Translation: imagewriter2/master
    Translate-URL: https://l10n.opensuse.org/projects/imagewriter2/master/id/

--- i18n/id.ts
+++ i18n/id.ts
@@ -6,17 +6,17 @@
     <message>
         <location filename="../AboutDialog.qml" line="18"/>
         <source>About openSUSE Image Writer</source>
-        <translation type="unfinished"></translation>
+        <translation>Tentang openSUSE Image Writer</translation>
     </message>
     <message>
         <location filename="../AboutDialog.qml" line="54"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Tentang</translation>
     </message>
     <message>
         <location filename="../AboutDialog.qml" line="63"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Pengaturan</translation>
     </message>
 </context>
 <context>
@@ -24,17 +24,17 @@
     <message>
         <location filename="../AboutView.qml" line="23"/>
         <source>openSUSE ImageWriter 2</source>
-        <translation type="unfinished"></translation>
+        <translation>openSUSE ImageWriter 2</translation>
     </message>
     <message>
         <location filename="../AboutView.qml" line="34"/>
         <source>Homepage: &lt;a href=&quot;%1&quot;&gt;On GitHub&lt;/a&gt;&lt;br/&gt;An &lt;a href=&quot;%2&quot;&gt;openSUSE&lt;/a&gt; project&lt;br/&gt;Created by Fabian Vogt</source>
-        <translation type="unfinished"></translation>
+        <translation>Laman depan: &lt;a href=&quot;%1&quot;&gt;Di GitHub&lt;/a&gt;&lt;br/&gt;Sebuah proyek &lt;a href=&quot;%2&quot;&gt;openSUSE&lt;/a&gt;&lt;br/&gt;Dibuat oleh Fabian Vogt</translation>
     </message>
     <message>
         <location filename="../AboutView.qml" line="58"/>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Tutup</translation>
     </message>
 </context>
 <context>
@@ -42,32 +42,32 @@
     <message>
         <location filename="../cdrecordburntask.cpp" line="7"/>
         <source>Writing to %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Menulis ke %1</translation>
     </message>
     <message>
         <location filename="../cdrecordburntask.cpp" line="36"/>
         <source>Unable to find the SCSI device</source>
-        <translation type="unfinished"></translation>
+        <translation>Tak dapat menemukan perangkat SCSI</translation>
     </message>
     <message>
         <location filename="../cdrecordburntask.cpp" line="61"/>
         <source>Aborted</source>
-        <translation type="unfinished"></translation>
+        <translation>Dibatalkan</translation>
     </message>
     <message>
         <location filename="../cdrecordburntask.cpp" line="69"/>
         <source>Writing successful</source>
-        <translation type="unfinished"></translation>
+        <translation>Penulisan berhasil</translation>
     </message>
     <message>
         <location filename="../cdrecordburntask.cpp" line="76"/>
         <source>Writing failed</source>
-        <translation type="unfinished"></translation>
+        <translation>Penulisan gagal</translation>
     </message>
     <message>
         <location filename="../cdrecordburntask.cpp" line="78"/>
         <source>Writing failed: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Penulisan gagal: %1</translation>
     </message>
 </context>
 <context>
@@ -75,57 +75,57 @@
     <message>
         <location filename="../gpgchecksumtask.cpp" line="12"/>
         <source>Downloading checksum</source>
-        <translation type="unfinished"></translation>
+        <translation>Mengunduh checksum</translation>
     </message>
     <message>
         <location filename="../gpgchecksumtask.cpp" line="45"/>
         <source>Failed to open GPG keyring</source>
-        <translation type="unfinished"></translation>
+        <translation>Gagal membuka GPG keyring</translation>
     </message>
     <message>
         <location filename="../gpgchecksumtask.cpp" line="57"/>
         <source>Download skipped, checksum verified</source>
-        <translation type="unfinished"></translation>
+        <translation>Pengunduhan dilewati, checksum terverifikasi</translation>
     </message>
     <message>
         <location filename="../gpgchecksumtask.cpp" line="71"/>
         <source>No network connectivity</source>
-        <translation type="unfinished"></translation>
+        <translation>Tak ada sambungan jaringan</translation>
     </message>
     <message>
         <location filename="../gpgchecksumtask.cpp" line="80"/>
         <source>Could not create target file</source>
-        <translation type="unfinished"></translation>
+        <translation>Tak dapat membuat berkas tujuan</translation>
     </message>
     <message>
         <location filename="../gpgchecksumtask.cpp" line="90"/>
         <source>Starting checksum download</source>
-        <translation type="unfinished"></translation>
+        <translation>Mulai mengunduh checksum</translation>
     </message>
     <message>
         <location filename="../gpgchecksumtask.cpp" line="108"/>
         <source>Aborted</source>
-        <translation type="unfinished"></translation>
+        <translation>Dibatalkan</translation>
     </message>
     <message>
         <location filename="../gpgchecksumtask.cpp" line="126"/>
         <source>Download failed: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Pengunduhan gagal: %1</translation>
     </message>
     <message>
         <location filename="../gpgchecksumtask.cpp" line="147"/>
         <source>Failed to verify signature of checksum</source>
-        <translation type="unfinished"></translation>
+        <translation>Gagal memverifikasi tanda tangan checksum</translation>
     </message>
     <message>
         <location filename="../gpgchecksumtask.cpp" line="152"/>
         <source>Download finished, could not rename</source>
-        <translation type="unfinished"></translation>
+        <translation>Pengunduhan selesai , tak dapat mengubah nama</translation>
     </message>
     <message>
         <location filename="../gpgchecksumtask.cpp" line="158"/>
         <source>Checksum downloaded and verified</source>
-        <translation type="unfinished"></translation>
+        <translation>Checksum telah diunduh dan diverifikasi</translation>
     </message>
 </context>
 <context>
@@ -133,62 +133,62 @@
     <message>
         <location filename="../imagedownloadtask.cpp" line="14"/>
         <source>Downloading %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Mengunduh %1</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="58"/>
         <source>%1 / %2 (%3/s)</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 / %2 (%3/s)</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="88"/>
         <source>Aborted</source>
-        <translation type="unfinished"></translation>
+        <translation>Dibatalkan</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="114"/>
         <source>Download failed: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Pengunduhan gagal: %1</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="134"/>
         <source>Download finished, but checksum failed</source>
-        <translation type="unfinished"></translation>
+        <translation>Pengunduhan selesai, tetapi checksum gagal</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="139"/>
         <source>Download finished, could not rename</source>
-        <translation type="unfinished"></translation>
+        <translation>Pengunduhan selesai, tak dapat mengubah nama</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="145"/>
         <source>Download done</source>
-        <translation type="unfinished"></translation>
+        <translation>Selesai mengunduh</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="162"/>
         <source>No checksum available for this image</source>
-        <translation type="unfinished"></translation>
+        <translation>Checksum tak tersedia untuk Citra ini</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="170"/>
         <source>Download skipped, found in cache</source>
-        <translation type="unfinished"></translation>
+        <translation>Pengunduhan dilewati, ditemukan dalam cache</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="178"/>
         <source>No network connectivity</source>
-        <translation type="unfinished"></translation>
+        <translation>Tak ada sambungan jaringan</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="187"/>
         <source>Could not create target file</source>
-        <translation type="unfinished"></translation>
+        <translation>Tak dapat membuat berkas tujuan</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="197"/>
         <source>Starting download</source>
-        <translation type="unfinished"></translation>
+        <translation>Mulai mengunduh</translation>
     </message>
 </context>
 <context>
@@ -196,27 +196,27 @@
     <message>
         <location filename="../imagedownloaderwritertask.cpp" line="39"/>
         <source>Downloading image</source>
-        <translation type="unfinished"></translation>
+        <translation>Mengunduh Citra</translation>
     </message>
     <message>
         <location filename="../imagedownloaderwritertask.cpp" line="60"/>
         <source>Writing image</source>
-        <translation type="unfinished"></translation>
+        <translation>Menulis Citra</translation>
     </message>
     <message>
         <location filename="../imagedownloaderwritertask.cpp" line="72"/>
         <source>Downloading failed</source>
-        <translation type="unfinished"></translation>
+        <translation>Gagal mengunduh</translation>
     </message>
     <message>
         <location filename="../imagedownloaderwritertask.cpp" line="99"/>
         <source>Done</source>
-        <translation type="unfinished"></translation>
+        <translation>Selesai</translation>
     </message>
     <message>
         <location filename="../imagedownloaderwritertask.cpp" line="104"/>
         <source>Writing failed</source>
-        <translation type="unfinished"></translation>
+        <translation>Penulisan gagal</translation>
     </message>
     <message>
         <location filename="../imagedownloaderwritertask.cpp" line="109"/>
@@ -229,37 +229,37 @@
     <message>
         <location filename="../metadatadownloadtask.cpp" line="9"/>
         <source>Getting information about %1 images</source>
-        <translation type="unfinished"></translation>
+        <translation>Mendapatkan informasi tentang Citra %1</translation>
     </message>
     <message>
         <location filename="../metadatadownloadtask.cpp" line="27"/>
         <source>Downloading metadata</source>
-        <translation type="unfinished"></translation>
+        <translation>Mengunduh metadata</translation>
     </message>
     <message>
         <location filename="../metadatadownloadtask.cpp" line="45"/>
         <source>Aborted</source>
-        <translation type="unfinished"></translation>
+        <translation>Dibatalkan</translation>
     </message>
     <message>
         <location filename="../metadatadownloadtask.cpp" line="69"/>
         <source>Metadata download failed, using cache</source>
-        <translation type="unfinished"></translation>
+        <translation>Pengunduhan metadata gagal, menggunakan cache</translation>
     </message>
     <message>
         <location filename="../metadatadownloadtask.cpp" line="75"/>
         <source>Metadata download failed, cache not available</source>
-        <translation type="unfinished"></translation>
+        <translation>Pengunduhan metadata gagal, cache tak tersedia</translation>
     </message>
     <message>
         <location filename="../metadatadownloadtask.cpp" line="87"/>
         <source>Metadata downloaded</source>
-        <translation type="unfinished"></translation>
+        <translation>Metadata terunduh</translation>
     </message>
     <message>
         <location filename="../metadatadownloadtask.cpp" line="94"/>
         <source>Failed to save metadata</source>
-        <translation type="unfinished"></translation>
+        <translation>Gagal menyimpan metadata</translation>
     </message>
 </context>
 <context>
@@ -267,22 +267,22 @@
     <message>
         <location filename="../qml64sizetype.h" line="42"/>
         <source>%L1 GiB</source>
-        <translation type="unfinished"></translation>
+        <translation>%L1 GB</translation>
     </message>
     <message>
         <location filename="../qml64sizetype.h" line="44"/>
         <source>%L1 MiB</source>
-        <translation type="unfinished"></translation>
+        <translation>%L1 MB</translation>
     </message>
     <message>
         <location filename="../qml64sizetype.h" line="46"/>
         <source>%L1 KiB</source>
-        <translation type="unfinished"></translation>
+        <translation>%L1 KB</translation>
     </message>
     <message>
         <location filename="../qml64sizetype.h" line="48"/>
         <source>%L1 B</source>
-        <translation type="unfinished"></translation>
+        <translation>%L1 B</translation>
     </message>
 </context>
 <context>
@@ -290,22 +290,22 @@
     <message>
         <location filename="../removabledevicesmodeludisks2.cpp" line="248"/>
         <source>%1 DVD at %2</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 DVD di %2</translation>
     </message>
     <message>
         <location filename="../removabledevicesmodeludisks2.cpp" line="251"/>
         <source>%1 Harddisk at %2</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 Harddisk di %2</translation>
     </message>
     <message>
         <location filename="../removabledevicesmodeludisks2.cpp" line="254"/>
         <source>%1 USB Drive at %2</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 USB Drive di %2</translation>
     </message>
     <message>
         <location filename="../removabledevicesmodeludisks2.cpp" line="257"/>
         <source>Unknown %1 device at %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Perangkat %1 tak diketahui di %2</translation>
     </message>
 </context>
 <context>
@@ -313,17 +313,17 @@
     <message>
         <location filename="../SettingsView.qml" line="32"/>
         <source>Only download images (this session only)</source>
-        <translation type="unfinished"></translation>
+        <translation>Hanya unduh Citra (sesi ini saja)</translation>
     </message>
     <message>
         <location filename="../SettingsView.qml" line="44"/>
         <source>Cache:</source>
-        <translation type="unfinished"></translation>
+        <translation>Cache:</translation>
     </message>
     <message>
         <location filename="../SettingsView.qml" line="61"/>
         <source>Clear cache</source>
-        <translation type="unfinished"></translation>
+        <translation>Bersihkan cache</translation>
     </message>
 </context>
 <context>
@@ -331,32 +331,32 @@
     <message>
         <location filename="../usbimagewritertask.cpp" line="73"/>
         <source>Writing %1 to %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Menulis %1 ke %2</translation>
     </message>
     <message>
         <location filename="../usbimagewritertask.cpp" line="102"/>
         <source>%1 / %2 (%3/s)</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 / %2 (%3/s)</translation>
     </message>
     <message>
         <location filename="../usbimagewritertask.cpp" line="108"/>
         <source>Starting write</source>
-        <translation type="unfinished"></translation>
+        <translation>Mulai menulis</translation>
     </message>
     <message>
         <location filename="../usbimagewritertask.cpp" line="127"/>
         <source>Aborted</source>
-        <translation type="unfinished"></translation>
+        <translation>Dibatalkan</translation>
     </message>
     <message>
         <location filename="../usbimagewritertask.cpp" line="150"/>
         <source>Writing successful</source>
-        <translation type="unfinished"></translation>
+        <translation>Berhasil menulis</translation>
     </message>
     <message>
         <location filename="../usbimagewritertask.cpp" line="155"/>
         <source>Writing failed: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Penulisan gagal: %1</translation>
     </message>
 </context>
 <context>
