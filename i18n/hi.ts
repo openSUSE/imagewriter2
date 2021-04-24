f5d2a70d54ca4a7202204ed92b8937394dc62f1d
Panwar <caspian7pena@gmail.com>
2021-04-24 11:43:58 +0000

    Added translation using Weblate (Hindi)

---
+++ i18n/hi.ts
@@ -0,0 +1,448 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hi">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../AboutDialog.qml" line="18"/>
+        <source>About openSUSE Image Writer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../AboutDialog.qml" line="54"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../AboutDialog.qml" line="63"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AboutView</name>
+    <message>
+        <location filename="../AboutView.qml" line="23"/>
+        <source>openSUSE ImageWriter 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../AboutView.qml" line="34"/>
+        <source>Homepage: &lt;a href=&quot;%1&quot;&gt;On GitHub&lt;/a&gt;&lt;br/&gt;An &lt;a href=&quot;%2&quot;&gt;openSUSE&lt;/a&gt; project&lt;br/&gt;Created by Fabian Vogt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../AboutView.qml" line="58"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CDRecordBurnTask</name>
+    <message>
+        <location filename="../cdrecordburntask.cpp" line="7"/>
+        <source>Writing to %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../cdrecordburntask.cpp" line="36"/>
+        <source>Unable to find the SCSI device</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../cdrecordburntask.cpp" line="61"/>
+        <source>Aborted</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../cdrecordburntask.cpp" line="69"/>
+        <source>Writing successful</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../cdrecordburntask.cpp" line="76"/>
+        <source>Writing failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../cdrecordburntask.cpp" line="78"/>
+        <source>Writing failed: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>GPGChecksumTask</name>
+    <message>
+        <location filename="../gpgchecksumtask.cpp" line="12"/>
+        <source>Downloading checksum</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gpgchecksumtask.cpp" line="45"/>
+        <source>Failed to open GPG keyring</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gpgchecksumtask.cpp" line="57"/>
+        <source>Download skipped, checksum verified</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gpgchecksumtask.cpp" line="71"/>
+        <source>No network connectivity</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gpgchecksumtask.cpp" line="80"/>
+        <source>Could not create target file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gpgchecksumtask.cpp" line="90"/>
+        <source>Starting checksum download</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gpgchecksumtask.cpp" line="108"/>
+        <source>Aborted</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gpgchecksumtask.cpp" line="126"/>
+        <source>Download failed: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gpgchecksumtask.cpp" line="147"/>
+        <source>Failed to verify signature of checksum</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gpgchecksumtask.cpp" line="152"/>
+        <source>Download finished, could not rename</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gpgchecksumtask.cpp" line="158"/>
+        <source>Checksum downloaded and verified</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ImageDownloadTask</name>
+    <message>
+        <location filename="../imagedownloadtask.cpp" line="14"/>
+        <source>Downloading %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../imagedownloadtask.cpp" line="58"/>
+        <source>%1 / %2 (%3/s)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../imagedownloadtask.cpp" line="88"/>
+        <source>Aborted</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../imagedownloadtask.cpp" line="114"/>
+        <source>Download failed: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../imagedownloadtask.cpp" line="134"/>
+        <source>Download finished, but checksum failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../imagedownloadtask.cpp" line="139"/>
+        <source>Download finished, could not rename</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../imagedownloadtask.cpp" line="145"/>
+        <source>Download done</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../imagedownloadtask.cpp" line="162"/>
+        <source>No checksum available for this image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../imagedownloadtask.cpp" line="170"/>
+        <source>Download skipped, found in cache</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../imagedownloadtask.cpp" line="178"/>
+        <source>No network connectivity</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../imagedownloadtask.cpp" line="187"/>
+        <source>Could not create target file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../imagedownloadtask.cpp" line="197"/>
+        <source>Starting download</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ImageDownloaderWriterTask</name>
+    <message>
+        <location filename="../imagedownloaderwritertask.cpp" line="39"/>
+        <source>Downloading image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../imagedownloaderwritertask.cpp" line="60"/>
+        <source>Writing image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../imagedownloaderwritertask.cpp" line="72"/>
+        <source>Downloading failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../imagedownloaderwritertask.cpp" line="99"/>
+        <source>Done</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../imagedownloaderwritertask.cpp" line="104"/>
+        <source>Writing failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../imagedownloaderwritertask.cpp" line="109"/>
+        <source>Deploying %1 to %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MetadataDownloadTask</name>
+    <message>
+        <location filename="../metadatadownloadtask.cpp" line="9"/>
+        <source>Getting information about %1 images</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../metadatadownloadtask.cpp" line="27"/>
+        <source>Downloading metadata</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../metadatadownloadtask.cpp" line="45"/>
+        <source>Aborted</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../metadatadownloadtask.cpp" line="69"/>
+        <source>Metadata download failed, using cache</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../metadatadownloadtask.cpp" line="75"/>
+        <source>Metadata download failed, cache not available</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../metadatadownloadtask.cpp" line="87"/>
+        <source>Metadata downloaded</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../metadatadownloadtask.cpp" line="94"/>
+        <source>Failed to save metadata</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../qml64sizetype.h" line="42"/>
+        <source>%L1 GiB</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml64sizetype.h" line="44"/>
+        <source>%L1 MiB</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml64sizetype.h" line="46"/>
+        <source>%L1 KiB</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml64sizetype.h" line="48"/>
+        <source>%L1 B</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RemovableDevicesModelUDisks2</name>
+    <message>
+        <location filename="../removabledevicesmodeludisks2.cpp" line="248"/>
+        <source>%1 DVD at %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../removabledevicesmodeludisks2.cpp" line="251"/>
+        <source>%1 Harddisk at %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../removabledevicesmodeludisks2.cpp" line="254"/>
+        <source>%1 USB Drive at %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../removabledevicesmodeludisks2.cpp" line="257"/>
+        <source>Unknown %1 device at %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsView</name>
+    <message>
+        <location filename="../SettingsView.qml" line="32"/>
+        <source>Only download images (this session only)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SettingsView.qml" line="44"/>
+        <source>Cache:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SettingsView.qml" line="61"/>
+        <source>Clear cache</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>USBImageWriterTask</name>
+    <message>
+        <location filename="../usbimagewritertask.cpp" line="73"/>
+        <source>Writing %1 to %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../usbimagewritertask.cpp" line="102"/>
+        <source>%1 / %2 (%3/s)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../usbimagewritertask.cpp" line="108"/>
+        <source>Starting write</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../usbimagewritertask.cpp" line="127"/>
+        <source>Aborted</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../usbimagewritertask.cpp" line="150"/>
+        <source>Writing successful</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../usbimagewritertask.cpp" line="155"/>
+        <source>Writing failed: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.qml" line="19"/>
+        <source>openSUSE Image Writer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="49"/>
+        <source>Could not load metadata</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="50"/>
+        <source>The downloaded Metadata was invalid.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="128"/>
+        <source>Source Image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="232"/>
+        <source>No valid image selected</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="242"/>
+        <source>No valid drive selected</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="249"/>
+        <source>The selected drive is too small for the image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="255"/>
+        <source>Ready to write!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="308"/>
+        <source>Overwrite data?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="309"/>
+        <source>By continuing here, you will LOSE ALL DATA on the Device
+%1!
+Are you sure?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="326"/>
+        <source>Could not start task</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="327"/>
+        <source>Opening of %1 for writing failed:
+%2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="349"/>
+        <source>Start!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="394"/>
+        <source>Target Disk</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="462"/>
+        <source>Task List</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="472"/>
+        <source>About openSUSE ImageWriter</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
