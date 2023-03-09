7a0213523f2b44a938249d710576f12340b99263
Moo oo <nazaq@protonmail.com>
2023-03-09 07:02:37 +0000

    Translated using Weblate (Lithuanian)
    Currently translated at 100.0% (81 of 81 strings)
    
    Translation: imagewriter2/master
    Translate-URL: https://l10n.opensuse.org/projects/imagewriter2/master/lt/

--- i18n/lt.ts
+++ i18n/lt.ts
@@ -90,42 +90,42 @@
     <message>
         <location filename="../gpgchecksumtask.cpp" line="71"/>
         <source>No network connectivity</source>
-        <translation type="unfinished"></translation>
+        <translation>Nėra tinklo ryšio</translation>
     </message>
     <message>
         <location filename="../gpgchecksumtask.cpp" line="80"/>
         <source>Could not create target file</source>
-        <translation type="unfinished"></translation>
+        <translation>Nepavyko sukurti paskirties failo</translation>
     </message>
     <message>
         <location filename="../gpgchecksumtask.cpp" line="90"/>
         <source>Starting checksum download</source>
-        <translation type="unfinished"></translation>
+        <translation>Pradedamas kontrolinės sumos atsiuntimas</translation>
     </message>
     <message>
         <location filename="../gpgchecksumtask.cpp" line="108"/>
         <source>Aborted</source>
-        <translation type="unfinished"></translation>
+        <translation>Nutraukta</translation>
     </message>
     <message>
         <location filename="../gpgchecksumtask.cpp" line="126"/>
         <source>Download failed: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Nepavyko atsisiųsti: %1</translation>
     </message>
     <message>
         <location filename="../gpgchecksumtask.cpp" line="147"/>
         <source>Failed to verify signature of checksum</source>
-        <translation type="unfinished"></translation>
+        <translation>Nepavyko patikrinti kontrolinės sumos parašo</translation>
     </message>
     <message>
         <location filename="../gpgchecksumtask.cpp" line="152"/>
         <source>Download finished, could not rename</source>
-        <translation type="unfinished"></translation>
+        <translation>Atsiuntimas baigtas, nepavyko pervadinti</translation>
     </message>
     <message>
         <location filename="../gpgchecksumtask.cpp" line="158"/>
         <source>Checksum downloaded and verified</source>
-        <translation type="unfinished"></translation>
+        <translation>Kontrolinė suma atsisiųsta ir patikrinta</translation>
     </message>
 </context>
 <context>
@@ -133,62 +133,62 @@
     <message>
         <location filename="../imagedownloadtask.cpp" line="14"/>
         <source>Downloading %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Atsiunčiama %1</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="58"/>
         <source>%1 / %2 (%3/s)</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 / %2 (%3/sek.)</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="88"/>
         <source>Aborted</source>
-        <translation type="unfinished"></translation>
+        <translation>Nutraukta</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="114"/>
         <source>Download failed: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Nepavyko atsisiųsti: %1</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="134"/>
         <source>Download finished, but checksum failed</source>
-        <translation type="unfinished"></translation>
+        <translation>Atsiuntimas baigtas, bet kontrolinė suma patyrė nesėkmę</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="139"/>
         <source>Download finished, could not rename</source>
-        <translation type="unfinished"></translation>
+        <translation>Atsiuntimas baigtas, nepavyko pervadinti</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="145"/>
         <source>Download done</source>
-        <translation type="unfinished"></translation>
+        <translation>Atsiuntimas atliktas</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="162"/>
         <source>No checksum available for this image</source>
-        <translation type="unfinished"></translation>
+        <translation>Šiam atvaizdžiui nėra prieinama kontrolinė suma</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="170"/>
         <source>Download skipped, found in cache</source>
-        <translation type="unfinished"></translation>
+        <translation>Atsiuntimas praleistas, rasta podėlyje</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="178"/>
         <source>No network connectivity</source>
-        <translation type="unfinished"></translation>
+        <translation>Nėra tinklo ryšio</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="187"/>
         <source>Could not create target file</source>
-        <translation type="unfinished"></translation>
+        <translation>Nepavyko sukurti paskirties failo</translation>
     </message>
     <message>
         <location filename="../imagedownloadtask.cpp" line="197"/>
         <source>Starting download</source>
-        <translation type="unfinished"></translation>
+        <translation>Pradedamas atsiuntimas</translation>
     </message>
 </context>
 <context>
@@ -196,32 +196,32 @@
     <message>
         <location filename="../imagedownloaderwritertask.cpp" line="39"/>
         <source>Downloading image</source>
-        <translation type="unfinished"></translation>
+        <translation>Atsiunčiamas atvaizdis</translation>
     </message>
     <message>
         <location filename="../imagedownloaderwritertask.cpp" line="60"/>
         <source>Writing image</source>
-        <translation type="unfinished"></translation>
+        <translation>Rašomas atvaizdis</translation>
     </message>
     <message>
         <location filename="../imagedownloaderwritertask.cpp" line="72"/>
         <source>Downloading failed</source>
-        <translation type="unfinished"></translation>
+        <translation>Atsiuntimas nepavyko</translation>
     </message>
     <message>
         <location filename="../imagedownloaderwritertask.cpp" line="99"/>
         <source>Done</source>
-        <translation type="unfinished"></translation>
+        <translation>Atlikta</translation>
     </message>
     <message>
         <location filename="../imagedownloaderwritertask.cpp" line="104"/>
         <source>Writing failed</source>
-        <translation type="unfinished"></translation>
+        <translation>Rašymas nepavyko</translation>
     </message>
     <message>
         <location filename="../imagedownloaderwritertask.cpp" line="109"/>
         <source>Deploying %1 to %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Išskleidžiama %1 į %2</translation>
     </message>
 </context>
 <context>
@@ -229,37 +229,37 @@
     <message>
         <location filename="../metadatadownloadtask.cpp" line="9"/>
         <source>Getting information about %1 images</source>
-        <translation type="unfinished"></translation>
+        <translation>Gaunama informacija apie %1 atvaizdžius</translation>
     </message>
     <message>
         <location filename="../metadatadownloadtask.cpp" line="27"/>
         <source>Downloading metadata</source>
-        <translation type="unfinished"></translation>
+        <translation>Atsiunčiami metaduomenys</translation>
     </message>
     <message>
         <location filename="../metadatadownloadtask.cpp" line="45"/>
         <source>Aborted</source>
-        <translation type="unfinished"></translation>
+        <translation>Nutraukta</translation>
     </message>
     <message>
         <location filename="../metadatadownloadtask.cpp" line="69"/>
         <source>Metadata download failed, using cache</source>
-        <translation type="unfinished"></translation>
+        <translation>Metaduomenų atsiuntimas nepavyko, naudojamas podėlis</translation>
     </message>
     <message>
         <location filename="../metadatadownloadtask.cpp" line="75"/>
         <source>Metadata download failed, cache not available</source>
-        <translation type="unfinished"></translation>
+        <translation>Metaduomenų atsiuntimas nepavyko, podėlis neprieinamas</translation>
     </message>
     <message>
         <location filename="../metadatadownloadtask.cpp" line="87"/>
         <source>Metadata downloaded</source>
-        <translation type="unfinished"></translation>
+        <translation>Metaduomenys atsisiųsti</translation>
     </message>
     <message>
         <location filename="../metadatadownloadtask.cpp" line="94"/>
         <source>Failed to save metadata</source>
-        <translation type="unfinished"></translation>
+        <translation>Nepavyko įrašyti metaduomenų</translation>
     </message>
 </context>
 <context>
@@ -267,22 +267,22 @@
     <message>
         <location filename="../qml64sizetype.h" line="42"/>
         <source>%L1 GiB</source>
-        <translation type="unfinished"></translation>
+        <translation>%L1 GiB</translation>
     </message>
     <message>
         <location filename="../qml64sizetype.h" line="44"/>
         <source>%L1 MiB</source>
-        <translation type="unfinished"></translation>
+        <translation>%L1 MiB</translation>
     </message>
     <message>
         <location filename="../qml64sizetype.h" line="46"/>
         <source>%L1 KiB</source>
-        <translation type="unfinished"></translation>
+        <translation>%L1 KiB</translation>
     </message>
     <message>
         <location filename="../qml64sizetype.h" line="48"/>
         <source>%L1 B</source>
-        <translation type="unfinished"></translation>
+        <translation>%L1 B</translation>
     </message>
 </context>
 <context>
@@ -290,22 +290,22 @@
     <message>
         <location filename="../removabledevicesmodeludisks2.cpp" line="248"/>
         <source>%1 DVD at %2</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 DVD ties %2</translation>
     </message>
     <message>
         <location filename="../removabledevicesmodeludisks2.cpp" line="251"/>
         <source>%1 Harddisk at %2</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 standusis diskas ties %2</translation>
     </message>
     <message>
         <location filename="../removabledevicesmodeludisks2.cpp" line="254"/>
         <source>%1 USB Drive at %2</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 USB diskas ties %2</translation>
     </message>
     <message>
         <location filename="../removabledevicesmodeludisks2.cpp" line="257"/>
         <source>Unknown %1 device at %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Nežinomas %1 įrenginys ties %2</translation>
     </message>
 </context>
 <context>
@@ -313,17 +313,17 @@
     <message>
         <location filename="../SettingsView.qml" line="32"/>
         <source>Only download images (this session only)</source>
-        <translation type="unfinished"></translation>
+        <translation>Tik atsisiųsti atvaizdžius (tik šį seansą)</translation>
     </message>
     <message>
         <location filename="../SettingsView.qml" line="44"/>
         <source>Cache:</source>
-        <translation type="unfinished"></translation>
+        <translation>Podėlis:</translation>
     </message>
     <message>
         <location filename="../SettingsView.qml" line="61"/>
         <source>Clear cache</source>
-        <translation type="unfinished"></translation>
+        <translation>Išvalyti podėlį</translation>
     </message>
 </context>
 <context>
@@ -331,32 +331,32 @@
     <message>
         <location filename="../usbimagewritertask.cpp" line="73"/>
         <source>Writing %1 to %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Rašomas %1 į %2</translation>
     </message>
     <message>
         <location filename="../usbimagewritertask.cpp" line="102"/>
         <source>%1 / %2 (%3/s)</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 / %2 (%3/sek.)</translation>
     </message>
     <message>
         <location filename="../usbimagewritertask.cpp" line="108"/>
         <source>Starting write</source>
-        <translation type="unfinished"></translation>
+        <translation>Pradedamas rašymas</translation>
     </message>
     <message>
         <location filename="../usbimagewritertask.cpp" line="127"/>
         <source>Aborted</source>
-        <translation type="unfinished"></translation>
+        <translation>Nutraukta</translation>
     </message>
     <message>
         <location filename="../usbimagewritertask.cpp" line="150"/>
         <source>Writing successful</source>
-        <translation type="unfinished"></translation>
+        <translation>Rašymas sėkmingas</translation>
     </message>
     <message>
         <location filename="../usbimagewritertask.cpp" line="155"/>
         <source>Writing failed: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Rašymas nepavyko: %1</translation>
     </message>
 </context>
 <context>
@@ -364,85 +364,88 @@
     <message>
         <location filename="../main.qml" line="19"/>
         <source>openSUSE Image Writer</source>
-        <translation type="unfinished"></translation>
+        <translation>openSUSE atvaizdžių rašymo programa</translation>
     </message>
     <message>
         <location filename="../main.qml" line="49"/>
         <source>Could not load metadata</source>
-        <translation type="unfinished"></translation>
+        <translation>Nepavyko įkelti metaduomenų</translation>
     </message>
     <message>
         <location filename="../main.qml" line="50"/>
         <source>The downloaded Metadata was invalid.</source>
-        <translation type="unfinished"></translation>
+        <translation>Atsisiųsti metaduomenys buvo netinkami.</translation>
     </message>
     <message>
         <location filename="../main.qml" line="128"/>
         <source>Source Image</source>
-        <translation type="unfinished"></translation>
+        <translation>Šaltinio atvaizdis</translation>
     </message>
     <message>
         <location filename="../main.qml" line="232"/>
         <source>No valid image selected</source>
-        <translation type="unfinished"></translation>
+        <translation>Nepasirinktas tinkamas atvaizdis</translation>
     </message>
     <message>
         <location filename="../main.qml" line="242"/>
         <source>No valid drive selected</source>
-        <translation type="unfinished"></translation>
+        <translation>Nepasirinktas tinkamas diskas</translation>
     </message>
     <message>
         <location filename="../main.qml" line="249"/>
         <source>The selected drive is too small for the image</source>
-        <translation type="unfinished"></translation>
+        <translation>Pasirinktas diskas yra per mažas šiam atvaizdžiui</translation>
     </message>
     <message>
         <location filename="../main.qml" line="255"/>
         <source>Ready to write!</source>
-        <translation type="unfinished"></translation>
+        <translation>Pasiruošę rašyti!</translation>
     </message>
     <message>
         <location filename="../main.qml" line="308"/>
         <source>Overwrite data?</source>
-        <translation type="unfinished"></translation>
+        <translation>Perrašyti duomenis?</translation>
     </message>
     <message>
         <location filename="../main.qml" line="309"/>
         <source>By continuing here, you will LOSE ALL DATA on the Device
 %1!
 Are you sure?</source>
-        <translation type="unfinished"></translation>
+        <translation>Jei tęsite, PRARASITE VISUS DUOMENIS, kurie yra įrenginyje
+%1!
+Ar esate tikri?</translation>
     </message>
     <message>
         <location filename="../main.qml" line="326"/>
         <source>Could not start task</source>
-        <translation type="unfinished"></translation>
+        <translation>Nepavyko pradėti užduoties</translation>
     </message>
     <message>
         <location filename="../main.qml" line="327"/>
         <source>Opening of %1 for writing failed:
 %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Nepavyko atverti %1, kad būtų rašoma:
+%2</translation>
     </message>
     <message>
         <location filename="../main.qml" line="349"/>
         <source>Start!</source>
-        <translation type="unfinished"></translation>
+        <translation>Pradėti!</translation>
     </message>
     <message>
         <location filename="../main.qml" line="394"/>
         <source>Target Disk</source>
-        <translation type="unfinished"></translation>
+        <translation>Paskirties diskas</translation>
     </message>
     <message>
         <location filename="../main.qml" line="462"/>
         <source>Task List</source>
-        <translation type="unfinished"></translation>
+        <translation>Užduočių sąrašas</translation>
     </message>
     <message>
         <location filename="../main.qml" line="472"/>
         <source>About openSUSE ImageWriter</source>
-        <translation type="unfinished"></translation>
+        <translation>Apie openSUSE ImageWriter</translation>
     </message>
 </context>
 </TS>
